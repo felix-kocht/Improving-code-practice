142517adb8e086a9621bcc80ae9c5d4ab07e5574
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2025-04-25 22:02:39 +0200

    display element added

--- task_manager.py
+++ task_manager.py
@@ -1,6 +1,6 @@
+import datetime
 from task import Task
 from typing import Optional
-import datetime
 
 
 def create_element(name: str, year: Optional[int] = None, month: Optional[int] = None, day: Optional[int] = None, completed: bool = False):
@@ -9,7 +9,16 @@ def create_element(name: str, year: Optional[int] = None, month: Optional[int] =
             date = datetime.date(year,month,day)
         else:
             date = None
-    except:
-        print("date seems to be invalid, task was not created")
-        return None
+    except ValueError as e:
+        raise ValueError("date seems to be invalid, task was not created") from e
     return Task(name,date,completed)
+
+def add_element(element: Task):
+    pass
+
+def display_element(element: Task, number: int):
+    if element.completed:
+        checked_symbol = "x"
+    else:
+        checked_symbol = "o"
+    print(f"No." + number + ": " + element.name + " " + checked_symbol + " due" + element.date)
--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -1,5 +1,5 @@
 import pytest
-from task_manager import create_element
+from task_manager import create_element, add_element, display_element
 
 def test_create_element_valid():
     task = create_element('first_task',2026,4,23)
@@ -16,5 +16,15 @@ def test_create_element_briefly():
 
 
 def test_create_element_invalid():
-    task = create_element('second_task', 0, 0, 0)
-    assert task == None
+    with pytest.raises(ValueError) as exc_info:
+        create_element("bad_task", 2024, 2, 30)
+    assert "invalid" in str(exc_info.value).lower()
+
+
+def test_display_element(capsys):
+    task = create_element('first_task', 2026, 4, 23) #TODO: find out if we create these from scratch every time
+    display_element(task, 4)
+    captured = capsys.readouterr()
+    assert "first_task" in captured.out
+    assert "2026" in captured.out
+    
